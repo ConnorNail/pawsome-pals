a97b2554a2392ce724de1761fc28e50242f9e5a3
Connor Nail <connor.nail@cox.net>
2023-04-13 22:28:43 -0700

    Added book pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,31 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Script from 'next/script'
-import { FaStar } from 'react-icons/fa';
+import Head from "next/head";
+import Image from "next/image";
+import Script from "next/script";
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
 
 export default function LandingPage() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleClick = () => {
+    setCurrentPage((prevPage) => (prevPage === 10 ? 1 : prevPage + 1));
+    console.log(currentPage)
+  };
+
   return (
     <div className="bg-gray-900 min-h-screen text-white">
       <Head>
         <title>Pawsome Pals Coloring Book</title>
         <link rel="icon" href="/favicon.ico" />
-        <noscript><img alt="facebook" height="1" width="1" style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=1500716207122439&ev=PageView&noscript=1"
-        /></noscript>
+        <noscript>
+          <img
+            alt="facebook"
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1500716207122439&ev=PageView&noscript=1"
+          />
+        </noscript>
       </Head>
       <Script id="fb-ads" strategy="afterInteractive">
         {`!function(f,b,e,v,n,t,s)
@@ -32,22 +46,25 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
               Pawsome Pals Coloring Book
             </h1>
             <p className="text-lg mb-6">
-              Relax and unwind with our dog-themed mandalas coloring book for kids
-              and adults. Our coloring book is perfect for animal lovers and anyone
-              who wants to improve their focus and reduce stress.
+              Relax and unwind with our dog-themed mandalas coloring book for
+              kids and adults. Our coloring book is perfect for animal lovers
+              and anyone who wants to improve their focus and reduce stress.
             </p>
-            <a href="https://www.amazon.com/Pawsome-Pals-Coloring-Dog-Themed-Mandalas/dp/B0C12DCTNC"
-              className="inline-block bg-purple-500 hover:bg-purple-700 text-white font-bold py-3 px-6 rounded-full">
+            <a
+              href="https://www.amazon.com/Pawsome-Pals-Coloring-Dog-Themed-Mandalas/dp/B0C12DCTNC"
+              className="inline-block bg-purple-500 hover:bg-purple-700 text-white font-bold py-3 px-6 rounded-full"
+            >
               Buy on Amazon
             </a>
           </div>
           <div className="md:w-1/2 flex justify-center items-center">
             <div className="aspect-[8/11.5] md:h-full w-full md:w-auto mt-5 md:mt-0 relative">
               <Image
-                src="/pawsome-pals.png"
-                alt="Pawsome Pals Coloring Book"
+                src={`/page-${currentPage}.png`}
+                alt={`Pawsome Pals Coloring Book Page ${currentPage}`}
                 fill
-                className="rounded-md object-cover"
+                className="rounded-md object-cover cursor-pointer"
+                onClick={handleClick}
               />
             </div>
           </div>
@@ -56,7 +73,10 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
           <h2 className="text-xl font-bold mb-2">What Customers Are Saying:</h2>
           <div className="">
             <p className="text-lg mr-4">
-              &quot;I just received my coloring book and was awe-struck by the images on the page. I cannot wait until work is over and I am able to add some color to the pups on each page! Thank you for creating this fun and beautiful book!&quot; - LoriAnne
+              &quot;I just received my coloring book and was awe-struck by the
+              images on the page. I cannot wait until work is over and I am able
+              to add some color to the pups on each page! Thank you for creating
+              this fun and beautiful book!&quot; - LoriAnne
             </p>
             <div className="flex items-center py-2">
               <FaStar className="text-yellow-500 mr-1" />
